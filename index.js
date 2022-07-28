1468542b36a350d51f882cb3d8ca59fff3553929
Ju <j.semeniuk.v@gmail.com>
2022-07-28 13:10:18 +0300

    add methods

--- index.js
+++ index.js
@@ -4,12 +4,22 @@ var cors = require("cors");
 
 const app = express();
 app.use(cors());
+app.use(express.urlencoded());
+app.use(express.json());
 
-const port = 3000;
+const port = 3001;
 
 app.get("/", (req, res) => {
   res.send(rockBands);
 });
+
+app.post("/", (req, res) => {
+  console.log(req.body);
+  res.send(req.body);
+});
+
+app.put;
+app.delete;
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
